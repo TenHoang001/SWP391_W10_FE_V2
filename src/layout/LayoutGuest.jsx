4e53edb16ec2ff3b73f2b057ac2c56c075746f4e
TienHoang <tienhtse160049@fpt.edu.vn>
2025-02-27 09:53:27 +0700

    tienht: update code

--- src/layout/LayoutGuest.jsx
+++ src/layout/LayoutGuest.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import Header from '../components/guest/Header';
+import Footer from '../components/guest/Footer';
 
 const LayoutGuest = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
